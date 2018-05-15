b48abb3ab977b0c26bf0e14339b1306644b4bc13
leosei <leosei@google.com>
2018-05-15 10:39:16 -0700

    Cleaned up Result processing (moved to Main class). Auth from file

---
+++ app/src/main/java/com/google/android/networth/BigQueryTask.java
@@ -0,0 +1,93 @@
+package com.google.android.networth;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.cloud.AuthCredentials;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
+import com.google.cloud.bigquery.FieldValue;
+import com.google.cloud.bigquery.Job;
+import com.google.cloud.bigquery.JobId;
+import com.google.cloud.bigquery.JobInfo;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryResponse;
+import com.google.cloud.bigquery.QueryResult;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+public class BigQueryTask extends AsyncTask<Context, Void, QueryResult> {
+    private final String CREDENTIALS_FILE = "AccountTools-3b0d75c06ec7.json";
+    private final String PROJECT_ID = "accounttools-1370";
+
+    private Context mContext;
+
+    @Override
+    protected QueryResult doInBackground(Context... contexts) {
+        QueryResult result = null;
+        this.mContext = contexts[0];
+
+        try {
+            AssetManager am = mContext.getAssets();
+            InputStream isCredentialsFile = am.open(CREDENTIALS_FILE);
+            Log.d("BQ","opened config file");
+            BigQuery bigquery = BigQueryOptions.builder()
+                    .authCredentials(AuthCredentials.createForJson(isCredentialsFile))
+                    .projectId( PROJECT_ID )
+                    .build().service();
+            Log.d("BQ","created bigquery service");
+
+            QueryJobConfiguration queryConfig =
+                    QueryJobConfiguration.builder(
+                            "SELECT "
+                                    + "Total "
+                                    + "FROM NetWorthTracker.Stats "
+                                    + "ORDER BY Date DESC "
+                                    + "LIMIT 2")
+                            .build();
+
+            Log.d("BQ","query ready");
+            // Create a job ID so that we can safely retry.
+            JobId jobId = JobId.of(UUID.randomUUID().toString());
+            Job queryJob = bigquery.create(JobInfo.builder(queryConfig).jobId(jobId).build());
+            Log.d("BQ","job running");
+
+            // Check for errors
+            if (queryJob == null) {
+                throw new RuntimeException("Job no longer exists");
+            } else if (queryJob.status().error() != null) {
+                // You can also look at queryJob.getStatus().getExecutionErrors() for all
+                // errors, not just the latest one.
+                Log.d("BQ","JOb issue");
+                throw new RuntimeException(queryJob.status().error().toString());
+            }
+
+            QueryResponse response = bigquery.getQueryResults(jobId);
+            result = response.result();
+
+
+        } catch (Exception e) {
+            Log.d("Main", "Exception: " + e.toString());
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Log.d("Main", "Launching BigQuery API request ");
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(QueryResult results) {
+        Log.d("bq", "results ready");
+
+    }
+
+}
--- app/src/main/java/com/google/android/networth/MainActivity.java
+++ app/src/main/java/com/google/android/networth/MainActivity.java
@@ -2,36 +2,77 @@ package com.google.android.networth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.cloud.bigquery.FieldValue;
+import com.google.cloud.bigquery.QueryResult;
+
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Credentials file: this file is stored in the assets/ directory. Replace it with yours.
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String projectId ="accounttools-1370";
+        TextView total = findViewById(R.id.networth);
+        TextView yesterday = findViewById(R.id.yesterday);
 
-
-        //Some url endpoint that you may have
-        String myUrl = "http://google.com";
-        //String to place our result in
-        String result = "" ;
-        //Instantiate new instance of our class
-        HTTPRequest getRequest = new HTTPRequest();
-        //Perform the doInBackground method, passing in our url
         try {
-            result = getRequest.execute(myUrl).get();
+            QueryResult result = new BigQueryTask().execute(MainActivity.this).get();
+            Log.d("MAIN VALUE",result.toString());
+            this.processResults(result,total,yesterday);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-
-        TextView networth = findViewById(R.id.networth);
-        networth.setText(result);
     }
+
+
+    private void processResults(QueryResult results, TextView total_view, TextView yesterday_view){
+        int count = 0;
+        double total = 0;
+        double total_yesterday=0;
+        Iterator<List<FieldValue>> results_iterator = results.iterateAll();
+        while(results_iterator.hasNext()) {
+            List<FieldValue> rows = results_iterator.next();
+            Log.d("ROW", "Total" + rows.toString());
+
+            if (count == 0) {
+                total = rows.get(0).doubleValue();
+            } else {
+                total_yesterday = rows.get(0).doubleValue();
+            }
+            count ++;
+        }
+
+        Log.d("VALUE", "Total: " +total);
+        Log.d("VALUE", "Yesterday's total: " +total_yesterday);
+
+        // Total today
+        DecimalFormat monetary_format = new DecimalFormat("###.#k Eur");
+        String total_formatted = monetary_format.format(total/1000);
+        String total_text = total_formatted;
+        total_view.setText(total_text);
+
+        // Delta from yesterday
+        DecimalFormat percent_format = new DecimalFormat("#.#");
+        double delta_percent = 100 * (total-total_yesterday) / total_yesterday;
+
+        String delta_percent_formatted = percent_format.format(delta_percent);
+        String total_yesterday_formatted = monetary_format.format(total_yesterday/1000);
+
+        String yesterday_text = "Yesterday: "+total_yesterday_formatted+" ("+delta_percent_formatted+"%)";
+        yesterday_view.setText(yesterday_text);
+    }
+
 }
